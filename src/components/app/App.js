862775939758851587ebe9e47f40997875c46c49
sem1on <sgorlov21@gmail.com>
2022-03-05 20:59:32 +0300

    Добавлены динамические импорты страниц

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react/cjs/react.production.min";
+
 
 import AppHeader from "../appHeader/AppHeader";
-import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
+import Spinner from "../spinner/Spinner";
 
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const Page404 = lazy(() => import('../pages/404'));
+const SingleComicPage = lazy(() => import('../pages/SinglComicPage'));
 
 const App = () => {
 
@@ -11,12 +17,15 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage/>}/>
-                        <Route path="/comics" element={<ComicsPage/>}/>
-                        <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-                        <Route path="*" element={<Page404/>}/>
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>}/>
+                            <Route path="/comics" element={<ComicsPage/>}/>
+                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
+                    
                 </main>
             </div>  
         </Router>
